251444415c92c606808c462e0632501a46807719
shuuuuuuuutya <fgmyra@yandex.ru>
2025-03-03 20:06:06 +0700

    new 030325

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Nikita'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'user'}
+    posts = [
+        {
+            'author': {'username': 'бэээ'},
+            'body': '5484743837!'
+        },
+        {
+            'author': {'username': 'бен'},
+            'body': 'Yes...'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+     flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+     return redirect(url_for('index'))
+  return render_template('login.html', title = 'Sing In', form=form)

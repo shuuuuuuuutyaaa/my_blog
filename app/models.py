672460519701ec3374778573bd6e0a98fb1ad4c4
shuuuuuuuutya <fgmyra@yandex.ru>
2025-03-10 18:32:31 +0700

    new 030325

--- app/models.py
+++ app/models.py
@@ -3,12 +3,29 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+
+@login.user_loader
+def load_user(id):
+  return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author'
+    )
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     #DUNDER METHOD
     def __repr__(self):
@@ -24,5 +41,5 @@ class Post(db.Model):
 
     author: so.Mapped[User] = so.relationship(back_populates='posts')
 
-    def repr(self):
+    def __repr__(self):
         return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,34 @@
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from app import db
+from app.models import User
+from flask import request
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'user'}
-    posts = [
-        {
-            'author': {'username': 'бэээ'},
-            'body': '5484743837!'
-        },
-        {
-            'author': {'username': 'бен'},
-            'body': 'Yes...'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    posts = []
+    return render_template('index.html', title='Home Page', posts=posts)
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-     flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-     return redirect(url_for('index'))
-  return render_template('login.html', title = 'Sing In', form=form)
+    user = db.session.scalar(
+      sa.select(User).where(User.username == form.username.data))
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or urlsplit(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
